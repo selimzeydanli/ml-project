f4edb0f4918624b1fe1962e45d9c9d23889f3fcb
Selim <selimzeydanli@gmail.com>
2024-05-07 17:15:25 +0300

    2 decimal rounding for lat lon and durations

--- src/LSTM.py
+++ src/LSTM.py
@@ -3,16 +3,16 @@ import torch
 import numpy as np
 import pandas as pd
 
-from torch import nn,optim
+from torch import nn, optim
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import transforms, datasets
 
 batchsize = 100
-training_data = datasets.   (/path/   , train = True, transform = transforms.ToTensor())
-test_data = datasets.  (/path/   train = Flase, transform = transforms.ToTensor())
+training_data = datasets.FashionMNIST(root="../fashion_mnist", train = True, transform = transforms.ToTensor(), download=True)
+test_data = datasets.FashionMNIST(root="../fashion_mnist", train = False, transform = transforms.ToTensor(), download=True)
 
 train_dataloader = DataLoader (training_data, batch_size = batchsize)
-test_dataloader = DataLoader (test_data, batchsize = batchsize)
+test_dataloader = DataLoader (test_data, batch_size = batchsize)
 
 # define hyperparameters
 sequence_len = 28
@@ -21,27 +21,29 @@ hidden_size = 128
 num_layers = 2
 num_classes = 10
 num_epochs = 5
-learning rate = 0.01
+learning_rate = 0.01
 
 class LSTM(nn.Module):
-    def __init__(selfself, input_len, hidden_size, num_class, num_layers):
+    def __init__(self, input_len, hidden_size, num_class, num_layers):
         super (LSTM, self). __init__()
         self.hidden_size  = hidden_size
         self.num_layers = num_layers
         self.lstm = nn. LSTM(input_len, hidden_size, num_layers, batch_first = True)
         self.output_layer = nn.Linear(hidden_size, num_classes)
 
-    def forward(selfself, X):
+    def forward(self, X):
         hidden_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
         cell_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
         out, _ = self.lstm(X, (hidden_states, cell_states))
-        out = self.output_layer(out([:, -1, :]))
+        out = self.output_layer(out[:, -1, :])
         return out
+
 model = LSTM(input_len, hidden_size, num_classes, num_layers)
 print(model)
 
 loss_func = nn.CrossEntropyLoss()
-optimizer = optim.SGD (model.paraeters(), lr=learning_rate)
+optimizer = optim.SGD (model.parameters(), lr=learning_rate)
+
 def train(num_epochs, model, train_dataloader, loss_func):
     total_steps = len (train_dataloader)
 
@@ -64,15 +66,14 @@ train(num_epochs, model, train_dataloader, loss_func)
 test_images, test_labels = next(iter(test_dataloader))
 test_labels
 
-test_output = model (test_images.view(-1, 28, 28))
+test_output = model(test_images.view(-1, 28, 28))
 
-predicted = torch.max(test_output, 1)
+predicted = torch.max(test_output, 1)[1]
 predicted
 
-correct = [1 for i in range(100) if predicted[i] == test_images[i]]
+correct = [1 for i in range(100) if predicted[i] == test_labels[i]]
 
 percentage_correct = sum(correct)/100
-
 percentage_correct
 
 def test_loop(dataloader, model, loss_fn, optimizer):
@@ -82,7 +83,7 @@ def test_loop(dataloader, model, loss_fn, optimizer):
 
     with torch.no_grad():
         for X, y in dataloader:
-            #reshapeimages
+            #reshape images
             X = X.reshape(-1, 28, 28)
             pred = model (X)
             test_loss += loss_fn(pred, y).item()
@@ -93,10 +94,4 @@ def test_loop(dataloader, model, loss_fn, optimizer):
     print (f"Test Error:\n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
     return 100*correct
 
-test_loop((test_dataloader, model, loss_func, adam))
-
-
-
-
-
-
+test_loop(test_dataloader, model, loss_func, optimizer)
